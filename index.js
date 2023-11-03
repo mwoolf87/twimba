b9d7a1ea0fd711b513b7a82f9e31f8b795cada6b
mwoolf87 <mwoolfdev@gmail.com>
2023-11-02 21:37:52 -0400

    added fontawesome CDN, added comment, heart and retweet icons to twimba boilerplate

--- index.js
+++ index.js
@@ -19,21 +19,28 @@ function getFeedHtml() {
                 <p class="handle">${tweet.handle}</p>
                 <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
-                    <span class="tweet-detail">
-                        ${tweet.replies.length}
-                    </span>
-                    <span class="tweet-detail">
-                        ${tweet.likes}
-                    </span>
-                    <span class="tweet-detail">
-                        ${tweet.retweets}
-                    </span>
-                </div>   
+                        <span class="tweet-detail">
+                        <i class="fa-regular fa-comment-dots"></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                        <i class="fa-solid fa-heart"></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                        <i class="fa-solid fa-retweet"></i>
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
             </div>            
     </div>
 </div>
     `;
-    return feedHTML;
   });
+  return feedHTML;
 }
-getFeedHtml();
+function render() {
+  document.getElementById("feed").innerHTML += getFeedHtml();
+}
+
+render();
