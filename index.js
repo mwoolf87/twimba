d481db5ec0a3c18e9474560c64cfd82f0e822671
mwoolf87 <mwoolfdev@gmail.com>
2023-11-02 21:01:30 -0400

    created getFeedHtml function for twimba boilerplate

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ tweetBtn.addEventListener("click", function () {
   console.log(tweetInput.value);
   tweetInput.value = "";
 });
+
+function getFeedHtml() {
+  let feedHTML = ``;
+  tweetsData.forEach(function (tweet) {
+    feedHTML += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                    <span class="tweet-detail">
+                        ${tweet.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweet.likes}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweet.retweets}
+                    </span>
+                </div>   
+            </div>            
+    </div>
+</div>
+    `;
+    return feedHTML;
+  });
+}
+getFeedHtml();
